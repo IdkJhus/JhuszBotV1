e75462f57c1ecd6809f16ced3221c96fc41aec23
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-02-20 14:03:15 -0600

    Update no digas eso papu.js

--- plugins/no digas eso papu.js
+++ plugins/no digas eso papu.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/a.mp3'
+let vn = './media/no digas eso papu.mp3'
 conn.sendFile(m.chat, vn, 'no digas eso papu.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
