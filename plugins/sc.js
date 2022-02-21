dd33ff76e6d27a88c993c45259f9ed83c6eced5e
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-02-20 23:25:39 -0600

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn, usedPrefix }) => {
 let who = m.sender
 let pp = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
 let kai = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
-let text = `*https://github.com/BrunoSobrino/ShadowBotV3*`.trim()
+let text = `*https://github.com/IdkJhus/JhuszBotV1*`.trim()
 conn.reply(m.chat, text, m, { thumbnail: kai, contextInfo: {
 mentionedJid: [who],
 externalAdReply: {
 title: '𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃',
-body: '🤖 Ｓｈａｄｏｗ Ｂｏｔ 🤖',
+body: '🤖 J H U S Z 🤖',
 thumbnail: pp
 }}})}
 handler.command = /^sc|script|repositorio|repobot|scbot$/i
