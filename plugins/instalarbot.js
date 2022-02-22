2727d7859bc6afc248655e207d321c97a2eec25f
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-02-21 21:37:52 -0600

    Update instalarbot.js

--- plugins/instalarbot.js
+++ plugins/instalarbot.js
@@ -1,5 +1,35 @@
 let handler = async m => m.reply(`
-Weno, esta es una versión modificada así que en YouTube encontraran el modo v:
+JhuszBot INSTALACION TERMUX OPCION 1 (GitHub)
+
+termux-setup-storage
+
+apt update && apt upgrade -y -y
+
+apt install git -y
+
+apt install nodejs -y
+
+apt install ffmpeg -y
+
+apt install imagemagick -y
+
+git clone https://github.com/IdkJhus/JhuszBotV1
+
+cd JhuszBotV1
+
+npm install
+
+npm install -g npm@8.5.0
+
+npm update
+
+npm start
+
+Para activar algunos comandos como el #añadir y #sacar el propietario del Bot debera usar el comando #enable restrict desde el numero que haya puesto en el archivo config.js
+
+Si instalas el Bot con GitHub para obtener nuevamente el codigo QR, escribe en el termux: 
+rm session.data.json 
+npm start
 `.trim()) 
 handler.help = ['instalarbot']
 handler.tags = ['info']
