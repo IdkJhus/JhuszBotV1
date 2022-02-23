db06d2614073b05894c6d42fcd4ede4a2144031c
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-02-22 20:49:18 -0600

    Update play1.js

--- plugins/play1.js
+++ plugins/play1.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, command, text }) => {
 handler.help = ['play', 'play2'].map(v => v + ' <canción >')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
-handler.group = true
+handler.group = false
 
 handler.exp = 0
 handler.registrar = false
