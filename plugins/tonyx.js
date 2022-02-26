e1d52e35ee2b3ec6a32f93e6081c6c37f61dca45
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-02-26 08:53:52 -0600

    Add files via upload

---
+++ plugins/tonyx.js
@@ -0,0 +1,16 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = `
+    *Tonyx OFC*
+    El ma jakel
+    ╔════⪼ཊ༼⚚༿𖤍༾⚚༽ཏ⪻════╗
+\n⍣𝑻𝒐𝒏𝒚𝒙|ɴʙᴊ ×͜×⍣\n
+Canal de YT: https://www.youtube.com/channel/UC_O1R_T3kc1Yn5ioJDWhiww
+El ma jakel de iutu oiste elmano
+    ╚════⪼ཊ༼⚚༿𖤍༾⚚༽ཏ⪻════╝
+    `.trim() 
+    
+    conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 JHUSZ X-X*', 'status@broadcast')
+    }
+    handler.command = /^(Tonyx.06|Tonyx|el ma jakel)$/i
+    
+    module.exports = handler
