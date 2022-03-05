b945e2025a7573c99729032b9e860f1e92ab2c56
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-03-05 16:22:12 -0600

    Add files via upload

--- plugins/nsfwloli.js
+++ plugins/nsfwloli.js
@@ -2,8 +2,8 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/nsfwloli?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©Jhusz X-X', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*¡Disfrutalo!*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
 }
-handler.command = /^(nsfwloli)$/i
+handler.command = /^(nsfwloli)$/i 
 
 module.exports = handler
