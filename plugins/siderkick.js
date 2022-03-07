f5fc25505ca12c59feb13a306fbcd88e293d0f3d
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-03-07 00:12:42 -0600

    Add files via upload

---
+++ plugins/siderkick.js
@@ -0,0 +1,55 @@
+let { MessageType, Presence } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, text, participants }) => {
+	await conn.updatePresence(m.chat, Presence.composing) 
+	let member = participants.map(u => u.jid)
+	if(!text) {
+		var sum = member.length
+	} else {
+		var sum = text
+	}
+	var total = 0
+
+	var sider = []
+	for(let i = 0; i < sum; i++) {
+		let users = m.isGroup ? participants.find(u => u.jid == member[i]) : {}
+		if((typeof global.DATABASE.data.users[member[i]] == 'undefined' || global.DATABASE.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
+			if (typeof global.DATABASE.data.users[member[i]] !== 'undefined'){
+				if(global.DATABASE.data.users[member[i]].whitelist == false){
+					total++
+					sider.push(member[i])
+				}
+			}else {
+				total++
+				sider.push(member[i])
+			}
+		}
+	}
+
+	if(total == 0) return conn.reply(m.chat, `*𝐌øɉɨŧø ha eliminado todos los fantasmas de este grupo:D.*`, m) 
+
+	for(let i = 0; i < sum; i++) {
+		let users = m.isGroup ? participants.find(u => u.jid == member[i]) : {}
+		if((typeof global.DATABASE.data.users[member[i]] == 'undefined' || global.DATABASE.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
+			if (typeof global.DATABASE.data.users[member[i]] !== 'undefined'){
+				if(global.DATABASE.data.users[member[i]].whitelist == false){
+					await conn.groupRemove(m.chat, [member[i]])
+				}
+			}else {
+				await conn.groupRemove(m.chat, [member[i]])
+			}
+		}
+	}
+	conn.reply(m.chat, `*Sacado con éxito a ${total} mirones.*\n_Inútiles_`, m)
+}
+handler.help = ['kicksider']
+handler.tags = ['group']
+handler.command = /^(kicksider)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+module.exports = handler
