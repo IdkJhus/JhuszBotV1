750d0933d244bd630daa718ddefa758500eb9f64
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-05-15 14:54:55 -0600

    Delete hentairandom.js

--- plugins/hentairandom.js
+++
@@ -1,9 +0,0 @@
-let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hentai By Jhusz X-X*', '©Jhusz X-X', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
-}
-handler.command = /^(hentai|hentay)$/i
-
-module.exports = handler
