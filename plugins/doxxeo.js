bbf617e413f7b1b5e006b033213c47b679485dca
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-05-15 15:20:06 -0600

    Update doxxeo.js

--- plugins/doxxeo.js
+++ plugins/doxxeo.js
@@ -1,22 +1,26 @@
-import { performance } from 'perf_hooks'
-let handler = async (m, { conn, text }) => {
-let start = `*😱 ¡¡Empezando Doxxeo!! 😱*`
-let boost = `*${pickRandom(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}%*`
-let boost2 = `*${pickRandom(['21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}%*`
-let boost3 = `*${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%*`
-let boost4 = `*${pickRandom(['61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80'])}%*`
-let boost5 = `*${pickRandom(['81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100'])}%*`
-await m.reply(start)
-await m.reply(boost)
-await m.reply(boost3)
-await m.reply(boost5)
-let old = performance.now()
-let neww = performance.now()
-let speed = `${neww - old}`
-let doxeo = `*_✅ Persona doxxeada con éxito_*\n\n*_Tiempo: ${speed} segundos!_*
+let { performance } = require('perf_hooks')
+
+let handler = async (m, { conn }) => {
+
+ let start = `*😱 ¡¡Empezando Doxxeo!! 😱*`
+ let boost = `${pickRandom(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}%`
+ let boost2 = `${pickRandom(['21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}%`
+ let boost3 = `${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%`
+ let boost4 = `${pickRandom(['61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80'])}%`
+ let boost5 = `${pickRandom(['81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100'])}%`
+
+   await m.reply(start)
+   await m.reply(boost)
+   await m.reply(boost2)
+   await m.reply(boost3)
+   await m.reply(boost4)
+   await m.reply(boost5)
+   let old = performance.now()
+   let neww = performance.now()
+   let speed = `${neww - old}`
+   let finish = `*_✅ Persona doxxeada con éxito_*\n\n*_Tiempo: ${speed} segundos!_*
 *RESULTADOS:*
-*Nombre:* ${text}
-*Ip:* 192.168.24.36
+*Ip:* 92.28.211.234
 *N:* 43 7462
 *W:* 12.4893
 *SS NUMBER:* 6979191519182016
@@ -51,12 +55,17 @@ Sof02s32inf14.1e100.net
 *Tcp:* 192.168.629-->92.28.211.167:8615
 *EXTERNAL MAC:* 6U:77:89:ER:O4
 *MODEM JUMPS:* 64`
-conn.reply(m.chat, doxeo, m)}
-handler.help = ['doxxear <nombre> | <@tag>']
-handler.tags = ['fun']
+
+     conn.reply(m.chat, finish, m)
+}
+handler.help = ['boost', 'refresh']
+handler.tags = ['info']
 handler.command = /^Doxxeo|doxxeo|doxxear|Doxxear|doxeo|doxear|doxxeame|doxeame/i
+
 handler.fail = null
-export default handler
+
+module.exports = handler
 
 function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+  return list[Math.floor(Math.random() * list.length)]
+}
