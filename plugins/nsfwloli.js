87ee960777033c142c8db33b3cf01d76fbc42b23
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-05-15 14:59:14 -0600

    Delete nsfwloli.js

--- plugins/nsfwloli.js
+++
@@ -1,9 +0,0 @@
-let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://server-api-rey.herokuapp.com/api/wallpaper/nsfwloli?apikey=apirey`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*¡Disfrutalo!*', 'Gata Dios', 'SIGUIENTE 🔄', `${usedPrefix + command}`, m, false)
-}
-handler.command = /^(nsfwloli)$/i 
-
-module.exports = handler
